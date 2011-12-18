17246d460bcc2d2d60f87c58938ed2b775be9465
Ian <ian.esling@gmail.com>
2011-12-18 00:41:32 +0000

    compiles

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,6 +29,7 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
     plugins {
-        compile ':heroku:1.0' ':cloud-support:1.0.8'
+        compile ':heroku:1.0'
+        compile ':cloud-support:1.0.8'
     }
 }
